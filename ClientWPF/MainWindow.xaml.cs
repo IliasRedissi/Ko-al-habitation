812e728d156f1cca767d3cbce551ee290786caf6
Ilias Redissi <ilias.redissi@gmail.com>
2016-03-18 16:29:08 +0100

    Récupération de la liste des biens

--- ClientWPF/MainWindow.xaml.cs
+++ ClientWPF/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Collections.ObjectModel;
+using ClientWPF.ServiceAgence;
 
 namespace ClientWPF
 {
@@ -23,6 +24,43 @@ namespace ClientWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        public ObservableCollection<BienImmobilierBase> BienImmobiliers { get;
+            set;
+        }
+
+        public MainWindow()
+        {
+            this.DataContext = this;
+            using (var client = new AgenceClient())
+            {
+                var criteres = new CriteresRechercheBiensImmobiliers
+                {
+                    DateMiseEnTransaction1 = null,
+                    DateMiseEnTransaction2 = null,
+                    DateTransaction1 = null,
+                    DateTransaction2 = null,
+                    EnergieChauffage = null,
+                    MontantCharges1 = -1,
+                    MontantCharges2 = -1,
+                    NbEtages1 = -1,
+                    NbEtages2 = -1,
+                    NbPieces1 = -1,
+                    NbPieces2 = -1,
+                    NumEtage1 = -1,
+                    NumEtage2 = -1,
+                    Prix1 = -1,
+                    Prix2 = -1,
+                    Surface1 = -1,
+                    Surface2 = -1,
+                    TransactionEffectuee = false,
+                    TypeBien = null,
+                    TypeChauffage = null,
+                    TypeTransaction =  null,
+                };
+                var resultat = client.LireListeBiensImmobiliers(criteres, null, null);
+
+                BienImmobiliers = resultat.SuccesExecution ? resultat.Liste.List : new ObservableCollection<BienImmobilierBase>();
+            }
+        }
     }
 }
