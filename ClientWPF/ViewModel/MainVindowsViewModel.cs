110f427094cf0e761fa62a9394db3816f2ea2688
Ilias Redissi <ilias.redissi@gmail.com>
2016-03-25 17:24:03 +0100

    Conversion base64 -> Image

--- ClientWPF/ViewModel/MainVindowsViewModel.cs
+++ ClientWPF/ViewModel/MainVindowsViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 using ClientWPF.Models;
 using ClientWPF.ServiceAgence;
 using Template_ListBox;
@@ -153,6 +157,19 @@ namespace ClientWPF.ViewModel
         private void ShowImmoDesc()
         {
             //ToDO Affichage de l'image
+        }
+
+        public Image Base64ToImage(string base64String)
+        {
+            byte[] binaryData = Convert.FromBase64String(base64String);
+
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.StreamSource = new MemoryStream(binaryData);
+            bi.EndInit();
+
+            var img = new Image {Source = bi};
+            return img;
         }
     }
 }
